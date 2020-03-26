547958267057cac5ea62de8cc9cae1cd6d418ce2
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-03-26 14:52:33 -0500

    initial components

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from '../Home/Home';
+import About from '../About/About';
+import Main from '../Main/Main';
+
+function App() {
+  return (<div className="App">
+
+    <Home />
+    <About />
+    <Main />
+
+  </div>);
+}
+
+export default App;
