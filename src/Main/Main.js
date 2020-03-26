a00d9c7287b9ec1af2763628a0e7c2662b10b2f8
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-03-26 15:45:54 -0500

    main route redirect

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 const Main = (props) => {
 
-  console.log(props);
+  console.log(props.match.params.id);
+
+  if(!props.match.params.id) {
+    // redirigir al usuario a la ruta /main/one
+    return <Redirect to="/main/one" />;
+  }
 
   return <div>
     <h1>Main</h1>
 
-    {!props.match.params.id && <p>default step</p>}
     {props.match.params.id === 'one' && <p>step one</p>}
     {props.match.params.id === 'two' && <p>step two</p>}
 
