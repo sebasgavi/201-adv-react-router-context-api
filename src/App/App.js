cde2eb21f58684f7a596c25ccdc53c95687a799d
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-03-26 15:13:10 -0500

    react router routes

--- src/App/App.js
+++ src/App/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import Home from '../Home/Home';
 import About from '../About/About';
 import Main from '../Main/Main';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+// cuando el usuario entre a la dirección / (root) queremos mostrarle el componente (o pantalla) Home
+// cuando el usuario entre a la dirección /about queremos mostrarle el componente About
 
 function App() {
   return (<div className="App">
+    <Router>
 
-    <Home />
-    <About />
-    <Main />
+      <Header />
 
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/main" component={Main} />
+
+      <Footer />
+
+    </Router>
   </div>);
 }
 
---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/main">Main</Link>
+    </nav>
+  </div>
+}
+
+export default Header;
