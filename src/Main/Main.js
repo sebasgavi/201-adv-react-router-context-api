a5ad92cb9e967c3b59034746af94f6b8f9d9f77b
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-03-26 15:57:51 -0500

    conditional steps redirect

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 const Main = (props) => {
+  const [ active, setActive ] = React.useState(false);
 
   console.log(props.match.params.id);
 
@@ -10,9 +11,28 @@ const Main = (props) => {
     return <Redirect to="/main/one" />;
   }
 
+  if(!active && props.match.params.id === 'two') {
+    // redirigir al usuario a la ruta /main/one
+    return <Redirect to="/main/one" />;
+  }
+
+  const handleActivateClick = () => {
+    setActive(true);
+  }
+
   return <div>
     <h1>Main</h1>
 
+    {!active && <button onClick={handleActivateClick}>Activate</button>}
+    {active && <p>Active</p>}
+
+    <nav>
+      <Link to="/main/one">Step One</Link>
+      <Link to="/main/two">Step Two</Link>
+    </nav>
+
+    {/* si no se usa redirect se puede hacer esta condición */}
+    {/* !props.match.params.id && <p>default step</p> */}
     {props.match.params.id === 'one' && <p>step one</p>}
     {props.match.params.id === 'two' && <p>step two</p>}
 
